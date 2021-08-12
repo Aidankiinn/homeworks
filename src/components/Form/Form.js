556aae64024b2831154fca748565ca8fa97ea6c1
Aidana Zhumakova <zhumakova99@gmail.com>
2021-08-12 15:07:10 +0600

    Added movieAdd and DeleteMovie functions

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import "./Form.css";
 
 class Form extends Component {
+    componentWillUnmount() {
+        console.log("Movie is deleted");
+    };
     render() {
-        const {placeholder,value,type, title, onInputChange, onBtnClick} = this.props;
+        const {placeholder,value,type, title, onInputChange, onDelete} = this.props;
         return (
             <div className="form">
                 <input value={value} type="text" placeholder={placeholder} onChange={onInputChange}/>
-                <button type={type} className="btn-add" onClick={onBtnClick}>{title}</button>
+                <button type={type} className={"btn-" + title} onClick={onDelete}>{title}</button>
             </div>
         );
     }
--- src/containers/MovieBuilder/MovieBuilder.js
+++ src/containers/MovieBuilder/MovieBuilder.js
@@ -9,39 +9,67 @@ class MovieBuilder extends Component {
         movieName: '',
     };
 
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('[Post] ShouldUpdate');
+    //     return nextProps !== this.props.title ||
+    //         nextProps.id !== this.props.id;
+    // }
+
+    componentWillUnmount() {
+        console.log("Movie part is not visible")
+    }
+
     takeMovie = (e) => {
         this.setState({
             movieName: e,
         })
-    }
+    };
 
     addMovie = (e) => {
+        const stateCopy = this.state;
+        const movieCopy = [...this.state.movie];
+        movieCopy.push({
+            title: this.state.movieName,
+            id: nanoid()
+        });
+        stateCopy.movie = movieCopy;
+        this.setState(stateCopy);
         e.preventDefault();
-        this.setState({
-            movie: this.state.movie.map(film => {
-                return {
-                    ...film,
-                    title: this.state.movieName,
-                    id: nanoid()
-                }
-            })
-        })
-        this.setState({
-            movieName: '',
-        })
+    };
+
+    deleteMovie = id => {
+        const stateCopy = this.state;
+        const movieCopy = [...this.state.movie];
+        const index = this.state.movie.findIndex((p => p.id === id));
+        movieCopy.splice(index, 1);
+        stateCopy.movie = movieCopy;
+        this.setState(stateCopy);
+    };
+
+    editMovie = (id, value) => {
+        const stateCopy = this.state;
+        const movieCopy = [...this.state.movie];
+        stateCopy.movie = movieCopy.map(m => {
+            if (id === m.id) {
+                return {...m, title: value}
+            }
+            return m;
+        });
+        this.setState(stateCopy);
     }
 
-
     render() {
+        console.log(this.state);
         return (
-            <form className="container">
-                <Form
-                    title="Add"
-                    placeholder="Add movie"
-                    type="submit"
-                    onInputChange={(e) => this.takeMovie(e.target.value)}
-                    onBtnClick={this.addMovie}
-                />
+            <div className="container">
+                <form onSubmit={this.addMovie}>
+                    <Form
+                        title="Add"
+                        placeholder="Add movie"
+                        type="submit"
+                        onInputChange={(e) => this.takeMovie(e.target.value)}
+                    />
+                </form>
                 <p>To watch list: </p>
                 {
                     this.state.movie.map(movie => (
@@ -50,11 +78,12 @@ class MovieBuilder extends Component {
                             key={movie.id}
                             type="button"
                             title="Delete"
+                            onDelete={() => this.deleteMovie(movie.id)}
+                            onInputChange={(e) => {this.editMovie(movie.id, e.target.value)}}
                         />
                     ))
                 }
-
-            </form>
+            </div>
         );
     }
 }
