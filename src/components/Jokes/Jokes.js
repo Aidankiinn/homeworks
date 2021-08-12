f237748f2809135b5e88fe88b0a4974f3eaa2f9e
Aidana Zhumakova <zhumakova99@gmail.com>
2021-08-12 16:35:28 +0600

    Created joke component

---
+++ src/components/Jokes/Jokes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Jokes = ({jokes}) => {
+    return <div>
+        {
+            jokes.map(joke => {
+                return (
+                    <div>
+                        <p><strong>Joke ID id: {joke.id}</strong></p>
+                        <hr/>
+                        <p>Joke is: {joke.value}</p>
+                        <hr/>
+                    </div>
+                );
+            })
+        }
+    </div>
+};
+
+export default Jokes;
