c2fd712e5c64dac648e8fa436b03f7fefdf3a781
Aidana Zhumakova <zhumakova99@gmail.com>
2021-08-22 17:44:59 +0600

    Downloaded Route dependency and used them.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
-import CountryBuilder from "./containers/CountryBuilder/CountryBuilder";
+import React from 'react';
+import "./App.css";
+import PortfolioBuilder from "./containers/PortfolioBuilder/PortfolioBuilder";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import MainPage from "./containers/MainPage/MainPage";
 
-
-class App extends Component {
-    render() {
-        return <CountryBuilder/>;
-    }
-}
+const App = () => (
+   <BrowserRouter>
+       <Switch>
+           <Route path="/" exact component={MainPage}/>
+           <Route path="/Portfolio" component={PortfolioBuilder}/>
+           <Route render={() => <h1>Not Found</h1>}/>
+       </Switch>
+   </BrowserRouter>
+);
 
 export default App;
