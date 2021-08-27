45c71e6af8e3724256bea11124e148e1c0ada468
Aidana Zhumakova <zhumakova99@gmail.com>
2021-08-27 22:56:50 +0600

    Downloaded axios and route dependencies.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import "./App.css";
-import PortfolioBuilder from "./containers/PortfolioBuilder/PortfolioBuilder";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import MainPage from "./containers/MainPage/MainPage";
+import PostBuilder from "./containers/PostBuilder/PostBuilder";
 
 const App = () => (
    <BrowserRouter>
        <Switch>
-           <Route path="/" exact component={MainPage}/>
-           <Route path="/Portfolio" component={PortfolioBuilder}/>
+           <Route path="/" component={PostBuilder}/>
            <Route render={() => <h1>Not Found</h1>}/>
        </Switch>
    </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from "axios";
-
-axios.defaults.baseURL = 'https://restcountries.eu/rest/v2/';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
