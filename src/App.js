8a2d93a5c263095a82369cf2ed4aa593ac267ad7
Aidana Zhumakova <zhumakova99@gmail.com>
2021-08-31 16:08:34 +0600

    Created full page Layout UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import PostBuilder from "./containers/PostBuilder/PostBuilder";
+import Layout from "./components/UI/Layout/Layout";
+import EditPage from "./containers/EditPage/EditPage";
 
 const App = () => (
-   <BrowserRouter>
-       <Switch>
-           <Route path="/" component={PostBuilder}/>
-           <Route render={() => <h1>Not Found</h1>}/>
-       </Switch>
-   </BrowserRouter>
+    <BrowserRouter>
+        <Layout>
+            <Switch>
+                <Route path="/" exact component={EditPage}/>
+                <Route path="/pages/admin" component={EditPage}/>
+                <Route path="/pages:id"  render={() => <h1>Not Found</h1>}/>
+                <Route render={() => <h1>Not Found</h1>}/>
+            </Switch>
+        </Layout>
+    </BrowserRouter>
 );
 
 export default App;
