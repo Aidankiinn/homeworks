76abca889e2fbecdf9c1ff1d280910675c5f51ce
Aidana Zhumakova <zhumakova99@gmail.com>
2021-09-05 13:18:01 +0600

    Created menu sidebar lists

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Drawer, makeStyles, Toolbar} from "@material-ui/core";
+import MenuList from "../MenuList/MenuList";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerContainer: {
+        overflow: 'auto',
+    },
+}));
+
+
+const Menu = () => {
+    const classes = useStyles();
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+                <MenuList/>
+            </div>
+        </Drawer>
+    );
+};
+
+export default Menu;
